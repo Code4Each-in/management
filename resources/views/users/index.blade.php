cbf785fccb69c4ed948b4b7081cf4d2d176216f9
arsh91 <arshsharma91@gmail.com>
2023-01-20 19:14:29 +0530

    worked on users create form

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -35,25 +35,59 @@
 	</div>
 </div>
 
+
+<!--end: Add department Modal -->
 <!--start: Add department Modal -->
-<div class="modal fade" id="addRole" tabindex="-1" aria-labelledby="role" aria-hidden="true">
+<div class="modal fade" id="addUsers" tabindex="-1" aria-labelledby="role" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="role">Add Role</h5>
+        <h5 class="modal-title" id="role">Add Users</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-		<form method="post" id="addDeaprtmentForm" action="">
+		<form method="post" id="addUsersForm" action="">
 		@csrf
-			<div class="modal-body">
-				<div class="mb-3">
-					<label for="role_name" class="form-label">Name</label>
-					<input type="text" class="form-control" id="role_name">
+		<div class="modal-body">
+			<div class="row">
+				<div class="col-lg-6">
+					<label for="first_name" class="form-label">FirstName</label>
+					<input type="text" class="form-control" id="users_name">
+				</div>
+				<div class="col-lg-6">
+					<label for="last_name" class="form-label">LastName</label>
+					<input type="text" class="form-control" id="users_name">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12">
+					<label for="email" class="form-label">Email</label>
+					<input type="text" class="form-control" id="users_name">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-6">
+					<label for="phone" class="form-label">Phone</label>
+					<input type="text" class="form-control" id="phone">
+				</div>
+				<div class="col-lg-6">
+					<label for="salary" class="form-label">Salary</label>
+					<input type="text" class="form-control" id="salary">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12">
+					<label for="password" class="form-label">Password</label>
+					<input type="text" class="form-control" id="password">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12">Address
+					<textarea name="address" class="form-control"  id="textarea"></textarea>
 				</div>
 			</div>
 			<div class="modal-footer">
 				<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-				<button type="button" class="btn btn-primary" onClick="addRole()" href="javascript:void(0)">Save</button>
+				<button type="button" class="btn btn-primary" onClick="addusers()" href="javascript:void(0)">Save</button>
 			</div>
 		</form>
     </div>
@@ -63,7 +97,23 @@
 @endsection
 @section('js_scripts')
     <script>
-       
+       $(document).ready(function(){
+			
+            $('#users_table').DataTable({
+                "order": []
+                //"columnDefs": [ { "orderable": false, "targets": 7 }]
+            });
+		$.ajaxSetup({
+				headers: {
+					'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+				}
+			});
+		});
+		
+			function openusersModal(){
+			$('#first_name').val('');
+			$('#addUsers').modal('show');
+		}
 		
 		
 	  </script>
