2eb3e206f5522aa2e4dff2bf8ba66ca16f264f22
arsh91 <arshsharma91@gmail.com>
2023-03-10 16:59:40 +0530

    change in users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('role_id')->constrained();
+            $table->integer('department_id')->constrained();
+            $table->tinyInteger('status')->default(0); 
             $table->rememberToken();
             $table->timestamps();
         });
