942a3eac2e35bc2dfc4d5bc3f2272a57213d0768
fuzail <fuzail@code4each.com>
2023-05-30 12:37:06 +0530

    changes for projects view section

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -11,14 +11,14 @@
                 {{ session()->get('message') }}
             </div>
             @endif   
-            <div class="row mb-2 mt-4">
+            <div class="row mb-1 mt-4">
 
                 <label for="" class="col-sm-3">Project Name</label>
                    <div class="col-sm-9">
                         {{$projects->project_name}}
                     </div>
                 </div>
-                 <div class="row mb-1">
+                 <div class="row mb-1 mt-4">
                     <label for="" class="col-sm-3"> Project Assigned</label>
                     <div class="col-sm-9" id="Projectsdata">
                         @foreach ($projectAssigns as $data)
@@ -31,19 +31,31 @@
                 <div class="row mb-1 mt-4">
                     <label for="edit_liveurl" class="col-sm-3 ">Live Url</label>
                     <div class="col-sm-9">
-                        {{$projects->live_url ?? '---'}}
+                        @if ($projects->live_url)
+                            <a href="{{$projects->live_url}}" target="_blank">{{$projects->live_url}}</a>
+                        @else
+                            {{'---'}}
+                        @endif
                     </div>
                 </div>
                 <div class="row mb-1 mt-4">
                     <label for="dev_liveurl" class="col-sm-3 ">Dev Url</label>
                     <div class="col-sm-9">
-                        {{$projects->dev_url ?? '---'}}
+                        @if ($projects->dev_url)
+                            <a href="{{$projects->dev_url}}" target="_blank">{{$projects->dev_url}}</a>
+                        @else
+                            {{'---'}}
+                        @endif
                     </div>
                 </div>
                 <div class="row mb-1 mt-4">
                     <label for="edit_gitrepo" class="col-sm-3 ">Git Repository</label>
                     <div class="col-sm-9">
-                        {{$projects->git_repo ?? '---'}}
+                        @if ($projects->git_repo)
+                            <a href="{{$projects->git_repo}}" target="_blank">{{$projects->git_repo}}</a>
+                        @else
+                            {{'---'}}
+                        @endif
                     </div>
                 </div>
                 <div class="row mb-1 mt-4">
@@ -52,7 +64,7 @@
                             {{$projects->tech_stacks ?? '---'}}
                     </div>
                 </div>
-                <div class="row mb-1">
+                <div class="row mb-1 mt-4">
                     <label for="tinymce_textarea" class="col-sm-3">Description</label>
                     <div class=" col-sm-9">
                              @if ($projects->description)
@@ -62,14 +74,14 @@
                                     @endif
                     </div>
                 </div>
-                <div class="row mb-3">
+                <div class="row mb-1 mt-4">
                                 <label for="edit_startdate" class="col-sm-3">Start Date</label>
                                 <div class="col-sm-9">
                                 {{date('d-m-Y', strtotime($projects->start_date));}}
 
                                 </div>
                 </div>
-                <div class="row mb-3">
+                <div class="row mb-1 mt-4">
                                 <label for="edit_enddate" class="col-sm-3">End Date</label>
                                 <div class="col-sm-9">
                                @if ($projects->end_date != Null)
@@ -81,7 +93,7 @@
                                 
                                 </div>
                 </div>
-                <div class="row mb-3">
+                <div class="row mb-1 mt-4">
                                 <label for="tinymce_textarea" class="col-sm-3">Credentials</label>
                                 <div class="col-sm-9">
                                     @if ($projects->credentials)
@@ -93,7 +105,7 @@
                                 </div>
                 </div>
                             
-                 <div class="row mb-1">
+                 <div class="row mb-1 mt-4">
                     <label for="edit_document" class="col-sm-3">Uploaded Documents</label>
                     <div class="col-sm-9" id="Projectsdata" style="margin:auto;">
                         @if (count($ProjectDocuments) < 1) 
@@ -134,7 +146,7 @@
                             @endif
                     </div>
                 </div> 
-                <div class="row mb-3">
+                <div class="row mb-1 mt-4">
                                 <label for="edit_status" class="col-sm-3">Status</label>
                                 <div class="col-sm-9">
                                 @if($projects->status == 'not_started')
