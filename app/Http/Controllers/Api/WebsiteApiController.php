484287fd01251d97cd74a0d78e33b60543268800
Arsh Sharma <arshsharma91@gmail.com>
2023-09-14 12:29:51 +0530

    create route for captcha

---
+++ app/Http/Controllers/Api/WebsiteApiController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Redirect;
+use App\Http\Controllers\Controller;
+
+class WebsiteApiController extends Controller
+{
+    public function getrandomchar()
+    {
+dd("yertrtt"); 
+    }
+
+   
+    }
+    
