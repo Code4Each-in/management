c6da08dfb6e5f72f7ecbfcfd50914954df75a364
nitin-0444 <nitin@code4each.com>
2025-04-01 15:08:38 +0530

    issue resolved in task  eta overdue

--- resources/views/scrumdash/index.blade.php
+++ resources/views/scrumdash/index.blade.php
@@ -56,30 +56,32 @@
                 <tr>
                     <th scope="col">Title</th>
                     <th scope="col">Assigned To</th>
-                    <th scope="col">ETA(d/m/y)</th> 
+                    <th scope="col">ETA (d/m/y)</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach($activetasks as $tasks)
                 @php
-                $eta = \Carbon\Carbon::parse($tasks->eta);
-                $isCloseToDeadline = $eta->diffInDays(\Carbon\Carbon::now()) <= 2 && $eta->isFuture();
+                    $eta = \Carbon\Carbon::parse($tasks->eta); 
+                    $currentTime = \Carbon\Carbon::now('Asia/Kolkata');  
+                    $isOverdue = $currentTime->toDateTimeString() > $eta->toDateTimeString();
                 @endphp
                 <tr>
                     <td>
-                        @if($isCloseToDeadline)
-                            <i class="fas fa-exclamation-circle" style="color: red;" title="Close to deadline"></i>
+                        @if($isOverdue)
+                            <i class="fas fa-exclamation-circle" style="color: red;" title="Task is overdue!"></i>
                         @endif
-                        <a href="{{ url('/view/ticket/'.$tasks->ticket_id)}}">
+                        <a href="{{ url('/view/ticket/'.$tasks->ticket_id) }}">
                             <i style="color:#4154f1;" class="pointer"></i>
-                        {{ $tasks->title }}</a>
+                            {{ $tasks->title }}
+                        </a>
                     </td>
                     <td>{{ $tasks->assigned_user_names }}</td>
                     <td>{{ $tasks->eta ? \Carbon\Carbon::parse($tasks->eta)->format('d/m/Y') : '---' }}</td>
-                </tr>            
+                </tr>
                 @endforeach
-            </tbody>            
-        </table>        
+            </tbody>
+        </table>                       
      </div>
    </div>
 </div>
