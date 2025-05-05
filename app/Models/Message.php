28ba480f06e57a2f99be1735d9e3e0253eff7db8
nitin-0444 <nitin@code4each.com>
2025-05-05 18:41:30 +0530

    message functionality

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -10,7 +10,18 @@ class Message extends Model
     use HasFactory, SoftDeletes;  
 
     protected $table = 'project_messages';  
-    protected $fillable = ['project_id', 'user_id', 'message', 'is_read', 'created_at', 'updated_at'];
+    protected $fillable = ['project_id', 'user_id', 'message', 'from','to','document', 'is_read', 'created_at', 'updated_at'];
     protected $dates = ['deleted_at'];
+
+
+    public function user()
+    {
+        return $this->belongsTo(Users::class);
+    }
+    public function project()
+    {
+        return $this->belongsTo(Projects::class);
+    }
+
 }
 
