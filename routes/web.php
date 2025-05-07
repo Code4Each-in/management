75c1329d12cd21d1ba082b497098d81936ec2e74
nitin-0444 <nitin@code4each.com>
2025-05-07 16:44:58 +0530

    delete route removed from middle ware

--- routes/web.php
+++ routes/web.php
@@ -216,7 +216,7 @@ Route::middleware(['role_permission'])->group(function () {
 
 	//Commnents Route Without Role Permission Middleware
 	Route::post('/add/comments/', [TicketsController::class, 'addComments'])->name('comments.add');
-	Route::delete('/comments/{id}/delete', [TicketsController::class, 'deleteComment'])->name('comments.delete');
+	
 	//Profiles Routes Without Role Permission Middleware
 	Route::get('profile', [UsersController::class, 'Userprofile'])->name('profile');
 	Route::post('/update/profile', [UsersController::class, 'updateProfile'])->name('update.profile');
@@ -322,5 +322,5 @@ Route::middleware(['role_permission'])->group(function () {
 	 Route::get('/get/project/messages/{projectId}', [MessageController::class, 'getMessagesByProject'])->name('get.project.messages');
 	 Route::delete('/comments/{id}/delete', [MessageController::class, 'destroy']);
 	 Route::post('/project-messages/{id}/mark-as-read', [MessageController::class, 'markAsRead'])->name('project-messages.markAsRead');
-
+	 Route::delete('/comments/{id}/delete', [TicketsController::class, 'deleteComment'])->name('comments.delete');
 	});
