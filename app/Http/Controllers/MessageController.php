f3c125daac44d9eb90e29b9744100d27d06cde44
riyathakur11 <riya@code4each.com>
2025-05-19 14:19:24 +0530

    email template work

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -162,7 +162,7 @@ public function addMessage(Request $request)
         try {
             // $rawMessage = $request->input('message');
             // $cleanMessage = strip_tags(html_entity_decode($rawMessage));
-            $messages["greeting-text"] = "{$name}!";
+            $messages["greeting-text"] = "Hello!";
             $messages["subject"] = "New Message received from - {$name}";
             $messages["title"] = "You've received a new message from <strong>{$name}</strong>.";
             $messages["body-text"] = " {$projectName}";
