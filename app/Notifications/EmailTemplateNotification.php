ffb476d93ba4b3589efc3cfde7efcd761ec6ffd4
arsh91 <arshsharma91@gmail.com>
2026-07-30 16:31:27 +0530

    issue fixed for email

--- app/Notifications/EmailTemplateNotification.php
+++ app/Notifications/EmailTemplateNotification.php
@@ -24,17 +24,55 @@ class EmailTemplateNotification extends Notification
         return ['mail'];
     }
 
-public function toMail($notifiable)
-{
-    return (new MailMessage)
-        ->subject($this->messages['subject'])
-        ->view('email_templates.mail_to_client', [
-            'client_name' => $this->messages['client_name'],
-            'subject'     => $this->messages['subject'],
-            'content'     => $this->messages['content'],
-            'banner_img'  => $this->messages['banner_img'],
-        ])
-        ->withSwiftMessage(function ($message) {
+    // public function toMail($notifiable)
+    // {
+    //     return (new MailMessage)
+    //         ->subject($this->messages['subject'])
+    //         ->view('email_templates.mail_to_client', [
+    //             'client_name' => $this->messages['client_name'],
+    //             'subject'     => $this->messages['subject'],
+    //             'content'     => $this->messages['content'],
+    //             'banner_img'  => $this->messages['banner_img'],
+    //         ])
+    //         ->withSwiftMessage(function ($message) {
+    //             \Log::info('CC/BCC debug', [
+    //                 'cc'  => $this->messages['cc_email'] ?? null,
+    //                 'bcc' => $this->messages['bcc_email'] ?? null,
+    //             ]);
+
+    //             $this->logo = $message->embed(
+    //                 Swift_Image::fromPath(public_path('assets/img/code4each_logo.png'))
+    //             );
+
+    //             if (!empty($this->messages['cc_email'])) {
+    //                 $cc = array_filter(array_map('trim', explode(',', $this->messages['cc_email'])));
+    //                 if (!empty($cc)) { $message->setCc($cc); }
+    //             }
+
+    //             if (!empty($this->messages['bcc_email'])) {
+    //                 $bcc = array_filter(array_map('trim', explode(',', $this->messages['bcc_email'])));
+    //                 if (!empty($bcc)) { $message->setBcc($bcc); }
+    //             }
+    //         });
+
+    // }
+    public function toMail($notifiable)
+    {
+        $mail = (new MailMessage)
+            ->subject($this->messages['subject'])
+            ->view('email_templates.mail_to_client', [
+                'client_name' => $this->messages['client_name'],
+                'subject'     => $this->messages['subject'],
+                'content'     => $this->messages['content'],
+                'banner_img'  => $this->messages['banner_img'],
+            ]);
+
+        // ✅ Set custom From address if provided
+        if (!empty($this->messages['from_email'])) {
+            $mail->from($this->messages['from_email'], $this->messages['from_name'] ?? null);
+        }
+
+        return $mail->withSwiftMessage(function ($message) {
             \Log::info('CC/BCC debug', [
                 'cc'  => $this->messages['cc_email'] ?? null,
                 'bcc' => $this->messages['bcc_email'] ?? null,
@@ -54,9 +92,7 @@ public function toMail($notifiable)
                 if (!empty($bcc)) { $message->setBcc($bcc); }
             }
         });
-
-}
-
+    }
 
     public function toArray($notifiable)
     {
